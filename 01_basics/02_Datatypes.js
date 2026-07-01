f28b8014bb0a428947c4aa3a9deb7effaaeb1cfc
Syed M Ahmed raza <syedahmedraza654@gmail.com>
2026-07-01 18:48:27 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+"use stric" // treat all Js code as newer version
+
+// alert( 3 + 3) // we are using node.js not browser
+
+console.log(3 + 3) // code readability sould be high
+
+console.log("Ahmed")
+
+let name = "Ahmed"
+let age = 15
+let isloggedIn = false
+let state;
+
+// number => 2 power 53
+// bignit
+// string => ""
+// bololan => true/false
+// null => standa lone value
+// undefind =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
